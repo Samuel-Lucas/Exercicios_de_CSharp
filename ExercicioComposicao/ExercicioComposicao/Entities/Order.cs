ade0ddc8917bf2456d55e1786a467c569bd8ada3
Samuel-Lucas <49723833+Samuel-Lucas@users.noreply.github.com>
2021-05-18 22:29:50 -0300

    Exercicios

---
+++ ExercicioComposicao/ExercicioComposicao/Entities/Order.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+using System.Collections.Generic;
+using ExercicioComposicao.Entities.Enum;
+
+namespace ExercicioComposicao.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+
+        public List<OrderItem> ItemList { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            this.Status = status;
+            Client = client;
+        }
+
+        public void addItem(OrderItem item)
+        {
+            ItemList.Add(item);
+        }
+
+        public void removeItem(OrderItem item)
+        {
+            ItemList.Remove(item);
+        }
+
+        public double total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in ItemList)
+            {
+                sum += item.subTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem item in ItemList)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ ExercicioStringBuilder/ExercicioStringBuilder/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExercicioStringBuilder.Entities;
+using System;
+
+namespace ExercicioStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip !");
+            Comment c2 = new Comment("Wow that's awesome !");
+
+            Post p1 = new Post(
+                    DateTime.Parse("21/06/2018 13:05:44"),
+                    "Travelling to New Zealand",
+                    "I'm going to visit this wonderful country",
+                    12
+                 );
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Console.WriteLine(p1);
+        }
+    }
+}
